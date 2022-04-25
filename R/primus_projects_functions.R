9efcba89643d17aae2597276d4cd1b44f3da6826
Primus AI <47700844+primus-ai@users.noreply.github.com>
2022-04-25 15:30:57 -0400

    Initial commit

---
+++ R/primus_projects_functions.R
@@ -0,0 +1,40 @@
+
+
+#' Primus Machine Learning Project Structure Function
+#'
+#'@param folders input folders to create. Default data, data exp, data prep, models, model_eval, reports, scripts, visualizations
+#'
+#' @return No return, just creates projects folders
+#' @importFrom fs dir_create
+#' @export
+#'
+#' @examples
+#'
+#' require(fs)
+#' primus_model_project_create()
+#'
+primus_model_project_create <- function(folders = c("00_data", "01_data_exp", "02_data_prep", "03_models", "04_model_eval", "05_reports", "06_scripts", "07_visualizations")) {
+    folders <- folders
+    dir_create(folders)
+    print(paste0("Folder created: ", folders))
+}
+
+#' Title
+#'
+#' @param folders input folders to remove
+#'
+#' @return
+#' @importFrom fs dir_delete
+#' @export
+#'
+#' @examples
+#'
+#' require(fs)
+#' primus_model_project_delete
+#'
+primus_model_project_delete <- function(folders = c("00_data", "01_data_exp", "02_data_prep", "03_models", "04_model_eval", "05_reports", "06_scripts", "07_visualizations")){
+    folders <- folders
+    dir_delete(folders)
+    print((paste0("This folder was deleted: ", folders)))
+}
+
